863bd9f857b240ab6c672ac6338458aad52ced7d
M Vendors <m-vendors@protonmail.com>
2021-05-23 20:49:07 +0300

    Initial Commit

---
+++ registration.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(
+    ComponentRegistrar::MODULE,
+    'perpetuum_mobile_DummyPkg',
+    __DIR__
+);
